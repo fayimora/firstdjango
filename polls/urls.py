e81ccc1004a95139ce0f3c0d5826543da367dc1c
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-25 01:18:58 +0000

    Add voting functionality

--- polls/urls.py
+++ polls/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     # ex: /polls/5/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 
-    # ex: /polls/5/votes
-    url(r'^(?P<question_id>[0-9]+)/votes/$', views.vote, name='vote')
+    # ex: /polls/5/vote
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
+
 ]
